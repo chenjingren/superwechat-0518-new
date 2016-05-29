9ab9da93aabd7f5d7398aa5d58c9a6c139c01e5f
chenjingren <1182508339@qq.com>
2016-05-29 20:16:52 +0800

    在cn.ucai.superwechat包下新建包task，在task中添加DownloadAllGroupTask、DownloadContactListTask、DownloadPublicGroupTask三个任务类发广播

---
+++ src/cn/ucai/superwechat/task/DownloadAllGroupTask.java
@@ -0,0 +1,63 @@
+package cn.ucai.superwechat.task;
+
+import android.content.Context;
+import android.content.Intent;
+
+import com.android.volley.Response;
+
+import java.util.ArrayList;
+
+import cn.ucai.superwechat.I;
+import cn.ucai.superwechat.SuperWeChatApplication;
+import cn.ucai.superwechat.activity.BaseActivity;
+import cn.ucai.superwechat.bean.Contact;
+import cn.ucai.superwechat.bean.Group;
+import cn.ucai.superwechat.data.ApiParams;
+import cn.ucai.superwechat.data.GsonRequest;
+import cn.ucai.superwechat.utils.Utils;
+
+/**
+ * Created by Administrator on 2016/5/23 0023.
+ */
+public class DownloadAllGroupTask extends BaseActivity {
+    private static final String TAG = DownloadAllGroupTask.class.getName();
+    Context mContext;
+    String userName;
+    String path;
+
+    public DownloadAllGroupTask(Context mContext, String userName) {
+        this.mContext = mContext;
+        this.userName = userName;
+        initPath();
+    }
+
+    private void initPath() {
+        try {
+            path = new ApiParams()
+                    .with(I.User.USER_NAME, userName)
+                    .getRequestUrl(I.REQUEST_DOWNLOAD_GROUPS);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void execute(){
+        executeRequest(new GsonRequest<Group[]>(path,Group[].class,
+                responseDownloadAllGroupTaskListener(),errorListener()));
+    }
+
+    private Response.Listener<Group[]> responseDownloadAllGroupTaskListener() {
+        return new Response.Listener<Group[]>() {
+            @Override
+            public void onResponse(Group[] response) {
+                if (response!=null){
+                    ArrayList<Group> groupList = SuperWeChatApplication.getInstance().getGroupList();
+                    ArrayList<Group> list = Utils.array2List(response);
+                    groupList.clear();
+                    groupList.addAll(list);
+                    mContext.sendStickyBroadcast(new Intent("update_group_list"));
+                }
+            }
+        };
+    }
+}
