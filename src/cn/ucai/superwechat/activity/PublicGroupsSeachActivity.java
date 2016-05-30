1bfb3879ec74a50fa379b854834630dd8561123e
chenjingren <1182508339@qq.com>
2016-05-30 23:27:14 +0800

    在PublicGroupsSearchActivity中添加搜索公共群组功能

--- src/cn/ucai/superwechat/activity/PublicGroupsSeachActivity.java
+++ src/cn/ucai/superwechat/activity/PublicGroupsSeachActivity.java
@@ -10,17 +10,31 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Response;
+import com.android.volley.toolbox.NetworkImageView;
 import com.easemob.EMError;
 import com.easemob.chat.EMGroup;
 import com.easemob.chat.EMGroupManager;
+
+import cn.ucai.superwechat.I;
 import cn.ucai.superwechat.R;
+import cn.ucai.superwechat.SuperWeChatApplication;
+import cn.ucai.superwechat.bean.Group;
+import cn.ucai.superwechat.data.ApiParams;
+import cn.ucai.superwechat.data.GsonRequest;
+import cn.ucai.superwechat.utils.UserUtils;
+
 import com.easemob.exceptions.EaseMobException;
 
 public class PublicGroupsSeachActivity extends BaseActivity{
     private RelativeLayout containerLayout;
     private EditText idET;
     private TextView nameText;
-    public static EMGroup searchedGroup;
+    public static Group searchedGroup;
+
+    private NetworkImageView niv_avatar;
+
+    ProgressDialog pd;
 
     @Override
     protected void onCreate(Bundle arg0) {
@@ -30,8 +44,12 @@ public class PublicGroupsSeachActivity extends BaseActivity{
         containerLayout = (RelativeLayout) findViewById(R.id.rl_searched_group);
         idET = (EditText) findViewById(R.id.et_search_id);
         nameText = (TextView) findViewById(R.id.name);
+
+        niv_avatar = (NetworkImageView) findViewById(R.id.avatar);
         
         searchedGroup = null;
+
+
     }
     
     /**
@@ -42,17 +60,28 @@ public class PublicGroupsSeachActivity extends BaseActivity{
         if(TextUtils.isEmpty(idET.getText())){
             return;
         }
-        
-        final ProgressDialog pd = new ProgressDialog(this);
+        pd = new ProgressDialog(this);
         pd.setMessage(getResources().getString(R.string.searching));
         pd.setCancelable(false);
         pd.show();
-        
-        new Thread(new Runnable() {
+
+        try {
+            String path = new ApiParams()
+                    .with(I.Group.HX_ID,idET.getText().toString())
+                    .getRequestUrl(I.REQUEST_FIND_PUBLIC_GROUP_BY_HXID);
+
+            executeRequest(new GsonRequest<Group>(path,Group.class,
+                    responseFindPublicGroupListener(),errorListener()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        /*new Thread(new Runnable() {
 
             public void run() {
                 try {
                     searchedGroup = EMGroupManager.getInstance().getGroupFromServer(idET.getText().toString());
+
                     runOnUiThread(new Runnable() {
                         public void run() {
                             pd.dismiss();
@@ -73,15 +102,35 @@ public class PublicGroupsSeachActivity extends BaseActivity{
                             }else{
                                 Toast.makeText(getApplicationContext(), getResources().getString(R.string.group_search_failed) + " : " + getString(R.string.connect_failuer_toast), Toast.LENGTH_SHORT).show();
                             }
+
+                            pd.dismiss();
                         }
                     });
                 }
             }
-        }).start();
-        
+        }).start();*/
     }
-    
-    
+
+    private Response.Listener<Group> responseFindPublicGroupListener() {
+        return new Response.Listener<Group>() {
+            @Override
+            public void onResponse(Group group) {
+                if (group!=null){
+                    searchedGroup =group;
+                    containerLayout.setVisibility(View.VISIBLE);
+                    nameText.setText(searchedGroup.getMGroupName());
+
+                    UserUtils.setGroupBeanAvatar(idET.getText().toString(),niv_avatar);
+                }else {
+                    containerLayout.setVisibility(View.GONE);
+                    Toast.makeText(getApplicationContext(), getResources().getString(R.string.group_not_existed), Toast.LENGTH_SHORT).show();
+                }
+                pd.dismiss();
+            }
+        };
+    }
+
+
     /**
      * 点击搜索到的群组进入群组信息页面
      * @param view
